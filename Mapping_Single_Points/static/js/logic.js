a4fe4a999f79a61f8a598e58672b6fff46b812da
tienl <tien.lee@gmail.com>
2020-08-05 19:48:19 -0700

    dfsafdsa

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,10 +6,10 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
 let marker =  L.circle([34.0522, -118.2437], {
-							   radius: 100
-							}).addTo(map);
+// 							   radius: 100
+// 							}).addTo(map);
 
-						 // L.marker([34.0522, -118.2437]).addTo(map);
+						  L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
