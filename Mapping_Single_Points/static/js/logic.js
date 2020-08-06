10031002d0317c1727ae856bfca3b4087f83456e
tienl <tien.lee@gmail.com>
2020-08-06 16:58:53 -0700

    13.4.2

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,5 +1,3 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid')
@@ -23,10 +21,11 @@ let marker =   //circleMarker radius is pixel (zoom won't change it)
 						  // L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer(
+	'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
+	{attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
-});
+	});
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
