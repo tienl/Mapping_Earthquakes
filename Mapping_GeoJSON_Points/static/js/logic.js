7f542882fe94f16a3f8ccfde5f91bf9ef5d97382
tienl <tien.lee@gmail.com>
2020-08-07 22:56:04 -0700

    13.5.4

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,54 +1,49 @@
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    // pointToLayer: function(feature, latlng) {
-    //   console.log(latlng);
-    //   return L.marker(latlng)
-    //   .bindPopup("<h3>" + feature.properties.name + "</h3>"
-    //             + `<hr style="height: 0px; border: none; border-top: 1px solid grey;">`
-    //             + `<h4> ${feature.properties.city}, ${feature.properties.country}</h4>`
-    //             );
-    // }
-    // onEachFeature
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3>"
-                   + `<hr style="height: 0px; border: none; border-top: 1px solid grey;">`
-                   + `<h3>Airport name: ${feature.properties.name}</h3>`
-                   );
-    }
-  }).addTo(map);
-
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v3/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
   });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [30, 30],
+	zoom: 2,
+	layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/tienl/Mapping_Earthquakes/master/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+      // We turn each feature into a marker on the map.
+      // onEachFeature
+      onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h3>Airport code: " + feature.properties.faa + "</h3>"
+                     + `<hr style="height: 0px; border: none; border-top: 1px solid grey;">`
+                     + `<h3>Airport name: ${feature.properties.name}</h3>`
+                     );
+      }
+    }).addTo(map);
+  });
